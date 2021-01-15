c5f33f5d58c118c2ce6185883c63c8a58933c22a
nancyiet <nancynancybansal@gmail.com>
2021-01-15 16:43:31 +0530

    Updates

---
+++ precache-manifest.4bad907b461939b86964bfaa1693cedb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "17100a76d4514f6dce2038e1cd451a9b",
+    "url": "/SfoodApi/index.html"
+  },
+  {
+    "revision": "b2c54fedddde9735fc08",
+    "url": "/SfoodApi/static/css/main.4b15c618.chunk.css"
+  },
+  {
+    "revision": "da17ceacbb82b7a51204",
+    "url": "/SfoodApi/static/js/2.d80a8c96.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/SfoodApi/static/js/2.d80a8c96.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b2c54fedddde9735fc08",
+    "url": "/SfoodApi/static/js/main.a5a31b87.chunk.js"
+  },
+  {
+    "revision": "f2302e60a006ca99fe39",
+    "url": "/SfoodApi/static/js/runtime-main.fcba8ef4.js"
+  }
+]);
